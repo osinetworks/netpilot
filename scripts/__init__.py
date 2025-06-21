74d3b27d2ee2cdd46d566faae6d74dcbc42b5b5d
Ozden <osicim@gmail.com>
2025-06-21 23:52:29 +0300

    Update version number to 1.9.2 in the Network Automation Suite

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # This file is part of the Network Automation Suite.
 
-VERSION = "1.8.3"
+VERSION = "1.9.2"
 
 def print_banner():
     print(f"Network Automation Suite v{VERSION}")
